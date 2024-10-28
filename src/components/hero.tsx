07e5bc64959d01e51e9e933a0331ff662dff87f3
Noel Emaas <nemaas@gbox.adnu.edu.ph>
2024-10-28 15:25:08 +0800

    added more details

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button"
 import { AtSign, Github, Linkedin } from "lucide-react";
+import flag from "@/assets/images/ph_flag.png";
 
 interface HeroProps {
   hidden?: boolean;
@@ -9,7 +10,13 @@ export default function Hero({ hidden = false }: HeroProps) {
     return (
       <div className={`${hidden ? 'hidden' : 'flex'} absolute top-0 w-full shadow-inner h-1/2 flex-col items-center md:justify-center justify-end md:pb-0 pb-[10%] bg-gradient-to-br from-[#1f1f1f] to-[#080808] z-10`}>
         <div className="container lg:px-[10%] sm:px-[5%] px-5 fixed pt-[5%]">
-          <h1 className='font-light text-gray-200 md:text-lg text-md'>Hi! I am <span className='text-white bold'>Noel Emaas.</span> </h1> 
+          <div className="">
+            <div className="flex group overflow-hidden w-[22px] duration-300 transition-all ease-in-out hover:w-[94px] cursor-pointer h-[20px] items-center gap-x-2 bg-gradient-to-br from-[#1f1f1f] to-[#181818] border border-[#2f2f2f] shadow-sm backdrop-blur-sm px-1 py-1 rounded-full">
+              <img src={flag} alt="flag" className="object-cover w-3 h-3 rounded-full" />
+              <p className="pr-2 text-xs transition-all duration-300 delay-200 opacity-0 group-hover:opacity-100">Philippines</p>
+            </div>
+            <h1 className='mt-2 font-light text-gray-200 md:text-lg text-md'>Hi! I am <span className='text-white bold'>Noel Emaas.</span> </h1> 
+          </div>
           <div className='z-40 flex flex-col w-full md:items-end md:justify-between md:flex-row'>
             <div className="sm:pt-[4%] pt-[10%] flex flex-col gap-y-1">
               <h1 className="text-xl lg:text-4xl md:text-3xl bold title drop-shadow-lg text-glow-white/40">
@@ -27,7 +34,7 @@ export default function Hero({ hidden = false }: HeroProps) {
                 <Github className="w-4 h-4 text-white"/>
               </Button>
               <Button 
-                className="shadow-sm group bg-gradient-to-r drop-shadow-lg to-[#2A3F60] from-[#50666a] border-0 border-none rounded-lg font-semibold hover:text-white text-sm gap-x-2 transition-all duration-300 ease-in-out"
+                className="shadow-sm group bg-gradient-to-r drop-shadow-lg to-[#2A3F60] border-glow-[#344245] from-[#50666a] border-0 border-none rounded-lg font-semibold hover:text-white text-sm gap-x-2 transition-all duration-300 ease-in-out"
                 variant="outline"
               >
                 <span className="w-0 overflow-hidden transition-all duration-300 ease-in-out opacity-0 group-hover:w-4 group-hover:opacity-100">
